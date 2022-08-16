631662a81a37d397fba885b403eb324e4492d85d
Ereur <amoussaouianas@gmail.com>
2022-08-16 18:05:13 +0100

    wa 7yad l mains o chrab lma

--- builtin/pwd.c
+++ builtin/pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zoukaddo <zoukaddo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aamoussa <aamoussa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 22:35:49 by zoukaddo          #+#    #+#             */
-/*   Updated: 2022/08/16 17:12:56 by zoukaddo         ###   ########.fr       */
+/*   Updated: 2022/08/16 18:04:13 by aamoussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,5 @@ int	built_in_pwd(char **str)
 	}
 	printf("%s\n",pwd);
 	free(pwd);
-	return (0);
-}
-
-int	main(void)
-{
-	char	*str[2];
-
-	str[0] = "pwd";
-	str[1] = NULL;
-	built_in_pwd(str);
 	return (0);
 }
