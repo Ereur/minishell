da5915530e1268aef9f3320ee56d7316c09a969a
Peannut <zakariaspam0@gmail.com>
2022-08-16 18:13:18 +0100

    gadit .h file

--- minishell.h
+++ minishell.h
@@ -1,17 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishel.h                                         :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aamoussa <aamoussa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zoukaddo <zoukaddo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 17:11:41 by aamoussa          #+#    #+#             */
-/*   Updated: 2022/08/16 17:14:22 by aamoussa         ###   ########.fr       */
+/*   Updated: 2022/08/16 18:12:05 by zoukaddo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHEL_H
 # define MINISHEL_H
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+int	built_in_pwd(char **str);
 
 #endif
