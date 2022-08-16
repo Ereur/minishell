a8c51bb9b3e3c5fe7d8b50775a8289052f844153
Peannut <zakariaspam0@gmail.com>
2022-08-16 18:14:14 +0100

    zdt header d libft

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: zoukaddo <zoukaddo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 17:11:41 by aamoussa          #+#    #+#             */
-/*   Updated: 2022/08/16 18:12:05 by zoukaddo         ###   ########.fr       */
+/*   Updated: 2022/08/16 18:13:54 by zoukaddo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include "libft/libft.h"
 
 int	built_in_pwd(char **str);
 
