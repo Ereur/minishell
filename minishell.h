001e858eb2018c0b40ed73e255fa4d6811f158d4
Ereur <amoussaouianas@gmail.com>
2022-08-16 18:14:13 +0100

    thala

--- minishell.h
+++ minishell.h
@@ -6,12 +6,13 @@
 /*   By: aamoussa <aamoussa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 17:11:41 by aamoussa          #+#    #+#             */
-/*   Updated: 2022/08/16 18:11:25 by aamoussa         ###   ########.fr       */
+/*   Updated: 2022/08/16 18:13:56 by aamoussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # include <unistd.h>
+# include <stdio.h>
 
 #endif
